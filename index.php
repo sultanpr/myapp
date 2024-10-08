3c25b4f37cb12212a487f75a7d20d91b57d8d112
sultanpr <shahsultan0912@gmail.com>
2024-10-08 15:55:16 +0600

    create router.php

--- index.php
+++ index.php
@@ -2,26 +2,4 @@
 
 require "functions.php";
 
-$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/contact' => 'controllers/contact.php'
-]; 
-
-if(array_key_exists($url, $routes)){
-    require $routes[$url]; 
-}else{
-    abort('404'); 
-}
-
-function abort($code){
-    http_response_code($code); 
-
-    require "views/{$code}.php";
-
-    die();
-}
+require "router.php";
