90ffb01a92e48b773f11575f423c938facb82646
sultanpr <shahsultan0912@gmail.com>
2024-10-08 17:47:02 +0600

    Database connection setup

---
+++ Database.php
@@ -0,0 +1,22 @@
+<?php
+
+class database{
+    public $connection; 
+
+    public function __construct($config, $user, $pass = '') {
+        
+        $dsn = "mysql:".http_build_query($config, '', ';');
+        $this->connection = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query){
+
+        $statement = $this->connection->prepare($query);
+        $statement->execute(); 
+        return $statement; 
+
+    }
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
-require "functions.php";
+// require "functions.php";
 
-require "router.php";
+// require "router.php";
+
+
+require "Database.php";
+$config = require ("config.php"); 
+
+$db = new database($config, 'root'); 
+$posts = $db->query('select * from posts');
+
+foreach($posts as $post){
+    echo "<li>".$post['title']."</li>";
+}
